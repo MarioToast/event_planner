42c30cb001da1cc977da5fb1e90264da2ec98c9d
MarioToast <mariokong@hotmail.no>
2024-11-26 05:47:32 -0500

    Commit test

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -6,6 +6,7 @@ const EventPlanner = () => {
         <div className="event-planner-container">
             <header>
                 <h1>Welcome to Event Planner</h1>
+                <h1>Just checkin' if I can still push.</h1>
             </header>
         </div>
     );
