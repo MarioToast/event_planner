5ce9372779301d49db00451820a6ec0529e2587a
MarioToast <mariokong@hotmail.no>
2024-11-26 06:14:53 -0500

    Did the thing

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './EventPlanner.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Testing</h1>
+            <div>
+                <p>&copy; Event Planner Organization. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
